dde18b7b056a0c0b1d1877a0af6367c769e050d9
alexavandusen <92638687+alexavandusen@users.noreply.github.com>
2025-06-09 13:04:28 +0100

    Create route_1.h

---
+++ Finalised Code/Drift/route_1.h
@@ -0,0 +1,16 @@
+// route.h
+#ifndef ROUTE_H
+#define ROUTE_H
+
+const double startLat = 51.54269676922407;
+const double startLon = -0.1557041893638124;
+
+const double stopLats[] = { 51.540530534165796, 51.538471245800736 };
+const double stopLons[] = { -0.1539519233273124, -0.15519040235353343 };
+
+const int numberOfStops = 2;
+
+#endif
+// 51.54269676922407, -0.1557041893638124
+// little one coffee --> princess of wales --> primrose corner --> shakespeare oak  --> regents park the hub  
+
